6f05419b265d40800a4aba3dc28a19abcc9e4d0b
MacbookPro <andreesito@gmail.com>
2017-12-24 14:36:35 -0800

    firebase db added and initialized

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,37 +1,55 @@
 'use strict';
-$(function(){
-	var elementArray = [];
-	var appContainer = $('#app');
-	var headLogo = $('<header>');
-	var main = $('<main>');
-	var searchBtn = $('<button>');
-	var createBtn = $('<button>');
-	main.attr('id', 'main');
-	searchBtn.attr('id', 'searchBtn');
-	createBtn.attr('id', 'createBtn');
-	headLogo.addClass('logo');
-	main.addClass('mainStyle');
-	searchBtn.addClass('btnStyle btn btn-lg active');
-	createBtn.addClass('btnStyle btn btn-lg active');
-	searchBtn.text('Search');
-	createBtn.text('Create Event');
-	main.append(searchBtn);
-	main.append(createBtn);
-	elementArray.push(headLogo);
-	elementArray.push(main);
-	
+$(function() {
 
-	appender(elementArray);
+    var config = {
+        apiKey: "AIzaSyD4zfoBJAqzq1qJNqAbW87-OlVXbOId8Hg",
+        authDomain: "open-leagues.firebaseapp.com",
+        databaseURL: "https://open-leagues.firebaseio.com",
+        projectId: "open-leagues",
+        storageBucket: "open-leagues.appspot.com",
+        messagingSenderId: "827491709161"
+    };
+    firebase.initializeApp(config);
+    var OLdatabase = firebase.database();
 
-	function appender(elements) {
 
-	for (var i = 0; i < elements.length; i++) {
-		appContainer.append(elements[i]);
-	}
+    var elementArray = [];
+    var appContainer = $('#app');
 
-	}
-	function emptyAppContainer() {
-		appContainer.empty();
-	}
+    landingPage();
+    appender(elementArray);
+
+    function landingPage() {
+        var headLogo = $('<header>');
+        var main = $('<main>');
+        var searchBtn = $('<button>');
+        var createBtn = $('<button>');
+        main.attr('id', 'main');
+        searchBtn.attr('id', 'searchBtn');
+        createBtn.attr('id', 'createBtn');
+        headLogo.addClass('logo');
+        main.addClass('mainStyle');
+        searchBtn.addClass('btnStyle btn btn-lg active');
+        createBtn.addClass('btnStyle btn btn-lg active');
+        searchBtn.text('Search');
+        createBtn.text('Create Event');
+        main.append(searchBtn);
+        main.append(createBtn);
+        elementArray.push(headLogo);
+        elementArray.push(main);
+
+    }
+
+    function appender(elements) {
+
+        for (var i = 0; i < elements.length; i++) {
+            appContainer.append(elements[i]);
+        }
+
+    }
+
+    function emptyAppContainer() {
+        appContainer.empty();
+    }
 
 });
