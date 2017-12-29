527eac7403c4210607f45fcd891049b811a4b350
MacbookPro <andreesito@gmail.com>
2017-12-28 20:46:14 -0800

    single marker placement functionality added / form added to create page

--- assets/js/app.js
+++ assets/js/app.js
@@ -18,9 +18,9 @@ $(function() {
     var map;
 
     landingPage();
-    appender(elementArray);
+    appender(elementArray, appContainer);
 
-// function that generates the landing page
+    // function that generates the landing page
     function landingPage() {
 
         emptyAppContainer();
@@ -42,24 +42,14 @@ $(function() {
             '</div>';
 
 
-
-
-        var headLogo = $('<header>');
-        var img = $('<img>');
-        var main = $('<main>');
-        var searchBtn = $('<button>');
-        var createBtn = $('<button>');
-        main.attr('id', 'main');
-        searchBtn.attr('id', 'searchBtn');
-        createBtn.attr('id', 'createBtn');
-        img.attr('src', './assets/css/sunroof.png')
-        headLogo.addClass('logo');
-        main.addClass('mainStyle overlay');
-        searchBtn.addClass('btnStyle btn btn-lg active');
-        createBtn.addClass('btnStyle btn btn-lg active');
+        var headLogo = $('<header class="logo">');
+        var img = $('<img src="./assets/css/sunroof.png">');
+        var main = $('<main id="main" class="mainStyle overlay">');
+        var searchBtn = $('<button id="searchBtn" class="btnStyle btn btn-lg active">');
+        var createBtn = $('<button id="createBtn" class="btnStyle btn btn-lg active">');
         searchBtn.text('Search');
         createBtn.text('Create Event');
-        headLogo.html(img);
+        headLogo.append(img);
         main.append(searchBtn);
         main.append(createBtn);
         elementArray.push(carousel);
@@ -68,50 +58,106 @@ $(function() {
 
     }
 
-//function that generates the create event page
+    //function that generates the create event page
     function createPage() {
 
         emptyAppContainer();
+        var childElements = [];
+        var main = $('<main class="createPageStyle">');
+        var mapContainer = $('<div id="map" class="createPageMap">');
+        var form = $('<form class="eventForm">');
+        var address = $('<h4>');
+        var sportsSelector = $('<select name="sports">');
+        var sportsOptions =
 
-        var main = $('<main>');
-        var mapContainer = $('<div>');
-        main.addClass('createPageStyle');
-        mapContainer.attr('id', 'map');
-        mapContainer.addClass('createPageMap');
+            '<option value="soccer">soccer</option>' +
+            '<option value="football">football</option>' +
+            '<option value="volleyball">volleyball</option>' +
+            '<option value="hockey">hockey</option>' +
+            '<option value="baseball">baseball</option>' +
+            '<option value="basketball">basketball</option>';
+
+        var labelStartTime = $('<label for="startTime">');
+        var gameStartTime = $('<input type="text" name="startTime">');
+        var labelDurationTime = $('<label for="DurationTime">');
+        var gameDurationTime = $('<input type="text" name="DurationTime">');
+        var labelTeamSize = $('<label for="teamSize">');
+        var teamSize = $('<input type="text" name="teamSize">');
+        var labelBenchSeats = $('<label for="benchSeats">');
+        var benchSeats = $('<input type="text" name="benchSeats">');
+        sportsSelector.html(sportsOptions);
+        labelStartTime.text('start time: ');
+        labelDurationTime.text('Duration: ');
+        labelTeamSize.text('Team size: ');
+        labelBenchSeats.text('bench seats: ');
+        childElements.push(
+
+            address,
+            sportsSelector,
+            labelStartTime,
+            gameStartTime,
+            labelDurationTime,
+            gameDurationTime,
+            labelTeamSize,
+            teamSize,
+            labelBenchSeats,
+            benchSeats);
+
+        appender(childElements, form);
+
         main.append(mapContainer);
+        main.append(form);
         appContainer.append(main);
         initMap();
     }
-// function that is used to append the elements of the landing page
-    function appender(elements) {
+
+    // function that is used to append the elements of the landing page
+    function appender(elements, container) {
 
         for (var i = 0; i < elements.length; i++) {
-            appContainer.append(elements[i]);
+            container.append(elements[i]);
         }
 
     }
-//function that empties the app div
+    //function that empties the app div
     function emptyAppContainer() {
         appContainer.empty();
     }
-// initialize google maps function
+    // initialize google maps function on create event page
     function initMap() {
 
         var marker;
+
         map = new google.maps.Map(document.getElementById('map'), {
             center: { lat: 32.852, lng: -117.185 },
             zoom: 9
         });
 
+        function placeMarker(location) {
+            if (marker) {
+                marker.setPosition(location);
+            } else {
+                marker = new google.maps.Marker({
+                    position: location,
+                    map: map
+                });
+            }
+
+        }
 
         google.maps.event.addListener(map, 'click', function(event) {
             var ltLg = event.latLng;
-            marker = new google.maps.Marker({ position: ltLg, map: map, title: 'test' });
+            placeMarker(ltLg);
+            var x = marker.getPosition().toString();
+            /*var s = x.formatted_address;*/
+            console.log(x);
+            console.log(typeof x);
+            /*console.log(s);*/
 
         });
 
     }
-// adds click handler onto the create event button - event delegation
+    // adds click handler onto the create event button - event delegation
     $('body').on('click', '#createBtn', createPage);
     /*$('body').on('click', '#searchBtn', searchPage);*/
 
