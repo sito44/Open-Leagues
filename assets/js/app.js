e7e74c6f271edf996db61a138fbf73e273160477
Laura Fleig <laurafleig@Lauras-MacBook-Pro.local>
2018-01-02 19:21:18 -0800

    still trying with this button

--- assets/js/app.js
+++ assets/js/app.js
@@ -79,14 +79,6 @@ $(function() {
 
         };
 
-        (function(d, s, id){
-            var js, fjs = d.getElementsByTagName(s)[0];
-            if (d.getElementById(id)) {return;}
-            js = d.createElement(s); js.id = id;
-            js.src = "https://connect.facebook.net/en_US/sdk.js";
-            fjs.parentNode.insertBefore(js, fjs);
-        }(document, 'script', 'facebook-jssdk'));
-
         function login() {
             FB.login(function(reponse) {
                 if (response.status === 'connected') {
